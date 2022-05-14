242ad18ea2f6c7e93f07c50644b7d186bcbd9817
Shivanshudeveloper <developer.shivanshu@gmail.com>
2022-05-14 14:45:34 +0530

    app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'a7b3cc31afddd4c8bf7b7e4e9476ceb5',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p'
 };
 
 /*
